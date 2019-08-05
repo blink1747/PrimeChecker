eff6e1dd140f8d53af811ee2e2395910982d6e13
Sujeet Poudel <apple.sujeetpoudel@gmail.com>
2019-08-05 12:01:43 -0500

    Code Added for Checking Prime Number using a function in View Controller

--- PrimeChecker/ViewController.swift
+++ PrimeChecker/ViewController.swift
@@ -10,6 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ResultText: UILabel!
+
+    @IBOutlet weak var TextInput: UITextField!
+    @IBAction func Button(_ sender: Any) {
+        
+        let input : String = TextInput.text!
+        let input_integer : Int = Int(input) ?? 0
+        
+        let result : Bool = primechecker(input_integer: input_integer)
+        
+        if result {
+            ResultText.text = "Prime"
+        } else {
+            ResultText.text = "Not Prime"
+        }
+    }
+    
+    func primechecker (input_integer : Int)->Bool {
+        var counter = 0
+        for n in 1...input_integer {
+            if input_integer%n == 0{
+                counter += 1
+            }
+        }
+        if counter == 2 {
+            return true
+        } else {
+            return false
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
