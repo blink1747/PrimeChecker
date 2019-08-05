74143e016d4636eaa01a14cdb73e989ba8d2266c
Sujeet Poudel <apple.sujeetpoudel@gmail.com>
2019-08-05 10:30:53 -0500

    Initial Commit

---
+++ PrimeChecker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PrimeChecker
+//
+//  Created by Sujeet Poudel on 8/5/19.
+//  Copyright © 2019 Sujeet Poudel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
